340578ab02da158e26034c986338c4084a2fac3b
Evan Thomas <evan.thomas@epfl.ch>
2023-10-30 15:59:18 +0100

    Add healthz route

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from config import config
 from models.config import KeycloakConfig
+from models.health import HealthCheck
 
 app = FastAPI()
 
@@ -14,3 +15,24 @@ async def get_keycloak_config() -> KeycloakConfig:
         realm=config.KEYCLOAK_REALM,
         url=config.KEYCLOAK_URL,
     )
+
+
+@app.get(
+    "/healthz",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
