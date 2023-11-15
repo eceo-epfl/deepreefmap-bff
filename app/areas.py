a715548d65bc8d078c1cc1c7ce1ca6d47a85a5fd
Evan Thomas <evan.thomas@epfl.ch>
2023-11-15 21:37:14 +0100

    Add sensor update/delete routes

--- app/areas.py
+++ app/areas.py
@@ -1,10 +1,9 @@
 from typing import Union, Any
-from fastapi import Depends, APIRouter, Query, Response, Body
+from fastapi import Depends, APIRouter, Query, Response, Body, Request
 from app.config import config
 from app.utils import get_async_client
 import httpx
 from uuid import UUID
-from pydantic.types import Json
 
 router = APIRouter()
 
@@ -15,6 +14,8 @@ async def get_area(
     *,
     area_id: UUID,
 ) -> Any:
+    """Get an area by id"""
+
     res = await client.get(
         f"{config.SOIL_API_URL}/v1/areas/{area_id}",
     )
@@ -24,6 +25,7 @@ async def get_area(
 
 @router.get("")
 async def get_areas(
+    request: Request,
     response: Response,
     *,
     filter: str = None,
@@ -31,15 +33,14 @@ async def get_areas(
     range: str = None,
     client: httpx.AsyncClient = Depends(get_async_client),
 ) -> Any:
-    # Fetch data from config.SOIL_API_URL/v1/areas/ and relay back to client
-
+    """Get all areas"""
     res = await client.get(
         f"{config.SOIL_API_URL}/v1/areas",
         params={"sort": sort, "range": range, "filter": filter},
     )
     response.headers["Access-Control-Expose-Headers"] = "Content-Range"
     response.headers["Content-Range"] = res.headers["Content-Range"]
-    print(res)
+
     return res.json()
 
 
@@ -49,7 +50,7 @@ async def create_area(
     client: httpx.AsyncClient = Depends(get_async_client),
 ) -> Any:
     """Creates an area"""
-    print(area)
+
     res = await client.post(
         f"{config.SOIL_API_URL}/v1/areas",
         json=area,
@@ -64,6 +65,8 @@ async def update_area(
     area: Any = Body(...),
     client: httpx.AsyncClient = Depends(get_async_client),
 ) -> Any:
+    """ "Updates an area by id"""
+
     res = await client.put(
         f"{config.SOIL_API_URL}/v1/areas/{area_id}", json=area
     )
@@ -77,6 +80,7 @@ async def delete_area(
     client: httpx.AsyncClient = Depends(get_async_client),
 ) -> None:
     """Delete an area by id"""
+
     res = await client.delete(f"{config.SOIL_API_URL}/v1/areas/{area_id}")
 
     return res.json()
--- app/sensors.py
+++ app/sensors.py
@@ -1,5 +1,5 @@
 from typing import Union, Any
-from fastapi import Depends, APIRouter, Query, Response
+from fastapi import Depends, APIRouter, Query, Response, Body
 from app.config import config
 from app.utils import get_async_client
 import httpx
@@ -14,6 +14,8 @@ async def get_sensor(
     *,
     sensor_id: UUID,
 ) -> Any:
+    """Get a sensor by id"""
+
     res = await client.get(
         f"{config.SOIL_API_URL}/v1/sensors/{sensor_id}",
     )
@@ -30,12 +32,55 @@ async def get_sensors(
     range: str = Query(None),
     client: httpx.AsyncClient = Depends(get_async_client),
 ) -> Any:
-    print("sort", sort, "range", range, "filter", filter)
+    """Get all sensors"""
+
     res = await client.get(
         f"{config.SOIL_API_URL}/v1/sensors",
         params={"sort": sort, "range": range, "filter": filter},
     )
     response.headers["Access-Control-Expose-Headers"] = "Content-Range"
     response.headers["Content-Range"] = res.headers["Content-Range"]
+
+    return res.json()
+
+
+@router.post("")
+async def create_sensor(
+    sensor: Any = Body(...),
+    client: httpx.AsyncClient = Depends(get_async_client),
+) -> Any:
+    """Creates an sensor"""
+    print(sensor)
+    res = await client.post(
+        f"{config.SOIL_API_URL}/v1/sensors",
+        json=sensor,
+    )
+
+    return res.json()
+
+
+@router.put("/{sensor_id}")
+async def update_sensor(
+    sensor_id: UUID,
+    sensor: Any = Body(...),
+    client: httpx.AsyncClient = Depends(get_async_client),
+) -> Any:
+    """ "Updates an sensor by id"""
+
+    res = await client.put(
+        f"{config.SOIL_API_URL}/v1/sensors/{sensor_id}", json=sensor
+    )
+
+    return res.json()
+
+
+@router.delete("/{sensor_id}")
+async def delete_sensor(
+    sensor_id: UUID,
+    client: httpx.AsyncClient = Depends(get_async_client),
+) -> None:
+    """Delete an sensor by id"""
+
+    res = await client.delete(f"{config.SOIL_API_URL}/v1/sensors/{sensor_id}")
 
     return res.json()
